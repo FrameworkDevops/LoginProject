3b96d64d92e6de6f812ee66bcf6792113e44177f
deepthi <deepthimogaparthi1207@gmail.com>
2018-12-21 11:41:32 +0530

    project with test cases

--- src/test/java/com/example/demo/LoginApplicationTests.java
+++ src/test/java/com/example/demo/LoginApplicationTests.java
@@ -12,6 +12,44 @@ public class LoginApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Test
+	public void testIndexPage() {
+	    beginAt("index.html"); 
+	    assertTitleEquals("Index");
+	    assertLinkPresent("Login");
+	    clickLink("Login");
+	    assertTitleEquals("Login");
+	}
+	
+	@Test
+	public void testLoginPage() {
+	    beginAt("login.html"); 
+	    assertTitleEquals("Login");
+	    assertLinkPresent("Index");
+	    clickLink("Index");
+	    assertTitleEquals("Index");
+	}
+
+	private void assertLinkPresent(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void assertTitleEquals(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void beginAt(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void clickLink(String string) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
 
